e4701a528c044e3f205eec25a5a9b14ec7ce3112
notTanveer <101289209+notTanveer@users.noreply.github.com>
2023-09-09 01:25:04 +0530

    version check

--- contracts/LegalRecords.sol
+++ contracts/LegalRecords.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.2 <0.9.0;
 
 contract LegalRecords {
     struct Record {
-        string content; 
+        string content;
         address owner;
     }
 
@@ -16,7 +16,7 @@ contract LegalRecords {
         require(bytes(content).length > 0, "Record content cannot be empty");
 
         Record memory newRecord = Record({
-            content: content, 
+            content: content,
             owner: msg.sender
         });
 
@@ -29,12 +29,10 @@ contract LegalRecords {
         return userRecords[msg.sender].length;
     }
 
-    function getLegalRecord(uint256 index) public view returns (string memory) {
+    function getLegalRecord(uint256 index) public view returns (string memory content) {
         require(index < userRecords[msg.sender].length, "Invalid record index");
 
-        Record storage record = userRecords[msg.sender][index];
-        emit RecordRetrieved(msg.sender, record.content);
-        
-        return record.content;
+        Record memory record = userRecords[msg.sender][index];
+        content = record.content;
     }
 }
